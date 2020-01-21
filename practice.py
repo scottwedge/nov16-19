c1f5ed407350e05eae5b4cf97e4d7f73ccf417fd
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 21:41:59 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print("hello")
-print("world")
-print()
+class TestAboveFreezing(nittest.TestCase):
+   """ Tests for temperature.above_freezing."""
+    
+      def test_above_freezing_above(self):
+          """Test a temperature that is above freezing."""
+          
+          expected = True 
+          actual = temperature.above_freezing(5.2)
+          self.assertEqual(expected, actual, "The temperature is above freezing.")
+          
+          
+      def test_above_freezing_below(self):
+          """Test a temperature that is below freezing."""
+          
+          expected = False
+          actual = temperature.above_freezing(-2)
+          self.assertEqual(expected, actual, 
+             "The temperature is below freezing")
