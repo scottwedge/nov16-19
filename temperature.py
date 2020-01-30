6da1b55e2467bd986c9150059659c3db45be9750
swedge <scott.wedge@gmail.com>
2020-01-30 15:13:07 -0500

    Fix typo. Create and import module.

---
+++ temperature.py
@@ -0,0 +1,8 @@
+"""Return True if parameter is greater than zero. Return False if less than zero"""
+
+def above_freezing(temp):
+    if temp > 0:
+        return True
+    else:
+        return False
+
